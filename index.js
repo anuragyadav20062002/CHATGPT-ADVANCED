d80975b2f95f970d4564fcd2581a3cd072b6efc2
Anurag Yadav <anuragyadav20062002@gmail.com>
2023-01-09 22:07:02 +0530

    added openai nodejs package

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-//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
+
+import { Configuration, OpenAIApi } from "openai";
+const configuration = new Configuration({
+    organization: "org-FzZoACkC609FvLP6TJV476ZH",
+    apiKey: "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",
+});
+const openai = new OpenAIApi(configuration);
+// const response = await openai.listEngines();
