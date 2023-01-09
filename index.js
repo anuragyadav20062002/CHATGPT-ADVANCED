7454cac63f64cebcf65f347e1c3d70972a2d31cd
Anurag Yadav <anuragyadav20062002@gmail.com>
2023-01-09 22:20:48 +0530

    created express API routes

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+const express = require('express')
 const { Configuration, OpenAIApi } = require("openai");
 const configuration = new Configuration({
     organization: "org-FzZoACkC609FvLP6TJV476ZH",
@@ -7,14 +7,22 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 // const response = await openai.listEngines();
 
-async function callApi(){  
-const response = await openai.createCompletion({
+const app = express()
+const port = 3000
+
+app.post('/', async (req,res)=>{
+const response = await   openai.createCompletion({
     model: "text-davinci-003",
     prompt: "Say this is a test",
     max_tokens: 7,
     temperature: 0,
   });
   console.log(response.data.choices[0].text)
-}
+  res.json({
+    data:response.data
+  })
+})
 
-callApi()
+app.listen(port,()=>{
+    console.log(`Example app listening at http://localhost:${port}`)
+})
