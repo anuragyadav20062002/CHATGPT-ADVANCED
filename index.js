81c353426554c5f7ae9347f25e3c79fee392156a
Anurag Yadav <anuragyadav20062002@gmail.com>
2023-01-09 22:14:06 +0530

    prepared openai config and api key

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 
-import { Configuration, OpenAIApi } from "openai";
+const { Configuration, OpenAIApi } = require("openai");
 const configuration = new Configuration({
     organization: "org-FzZoACkC609FvLP6TJV476ZH",
-    apiKey: "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",
+    apiKey: "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",
 });
 const openai = new OpenAIApi(configuration);
 // const response = await openai.listEngines();
+
+async function callApi(){  
+const response = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: "Say this is a test",
+    max_tokens: 7,
+    temperature: 0,
+  });
+  console.log(response.data.choices[0].text)
+}
+
+callApi()
